34c77cc194e6e9c0f6cc6851e275e9400b062a7b
Nic-Mon <n_mon@berkeley.edu>
2017-04-26 13:40:08 -0700

    fixed indent x3

--- dstk/complex_num.py
+++ dstk/complex_num.py
@@ -23,7 +23,7 @@ class complex_num():
 	def __eq__(self, other):
 		"""Override the default Equals behavior"""
 		if isinstance(other, self.__class__):
-		return self.__dict__ == other.__dict__
+			return self.__dict__ == other.__dict__
 		return False
 
 	def __ne__(self, other):
