9a171f9674113f24159d5160f6b532b8d1594f71
Nic-Mon <n_mon@berkeley.edu>
2017-04-25 22:33:17 -0700

    fixed

--- tests/test_complex.py
+++ tests/test_complex.py
@@ -5,12 +5,12 @@ def test_canCreateComplex():
 
 def test_absFunctionsCorrectlyOnRealOnly():
 	four = complex(4,0)
-	assertEqual(four.abs(), 4)
+	assert four.abs() == 4
 
 def test_absFunctionsCorrectlyOnImaginaryOnly():
 	Ithree = complex(0,3)
-	assertEqual(3, Ithree.abs())
+	assert 3 == Ithree.abs()
 
 def test_absFunctionsCorrectly():
 	num = complex(3,4)
-	assertEqual(num.abs(), 5)
+	assert num.abs() == 5
