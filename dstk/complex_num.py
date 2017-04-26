77d98be3e1cb2e9677e7cc32447e38854c0f0680
Nic-Mon <n_mon@berkeley.edu>
2017-04-26 12:57:20 -0700

    added equals override

--- dstk/complex_num.py
+++ dstk/complex_num.py
@@ -19,3 +19,14 @@ class complex_num():
 		self.real -= other_complex.real
 		self.imaginary -= other_complex.imaginary
 		return self
+
+	def __eq__(self, other):
+    """Override the default Equals behavior"""
+    	if isinstance(other, self.__class__):
+        return self.__dict__ == other.__dict__
+    	return False
+
+    def __ne__(self, other):
+    	"""Define a non-equality test"""
+    	return not self.__eq__(other)
+
