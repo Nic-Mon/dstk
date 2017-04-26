945244da1b19765efd70a5d93e06ff6cbaae3fd8
Nic-Mon <n_mon@berkeley.edu>
2017-04-25 22:27:22 -0700

    added complex abs test

--- tests/test_complex.py
+++ tests/test_complex.py
@@ -2,3 +2,15 @@ import pytest
 
 def test_canCreateComplex():
 	two = complex(2, 0)
+
+def test_absFunctionsCorrectlyOnRealOnly():
+	four = complex(4,0).
+	assertEquals(four.abs(), 4)
+
+def test_absFunctionsCorrectlyOnImaginaryOnly():
+	Ithree = complex(0,3)
+	assertEquals(3, Ithree.abs())
+
+def test_absFunctionsCorrectly():
+	num = complex(3,4)
+	assertEquals(num.abs(), 5)
