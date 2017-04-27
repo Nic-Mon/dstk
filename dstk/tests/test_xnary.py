775307d706cc8bad20935f61e124dc78fb5bfc64
Nic-Mon <n_mon@berkeley.edu>
2017-04-27 13:42:59 -0700

    fixed test syntax

--- dstk/tests/test_xnary.py
+++ dstk/tests/test_xnary.py
@@ -7,10 +7,10 @@ def test_canInstantiateBinary():
 def test_canInstantiate5nary():
 	fivenary = xnary(5,234)
 
-def test_binaryToString:
+def test_binaryToString():
 	binary_fifty = xnary(2,50)
 	assert(str(binary_fifty) == '110010')
 
-def test_4narytoString:
+def test_4narytoString():
 	fournary_fortytwo = xnary(4,242)
 	assert(str(fournary_fortytwo) == '3302')
