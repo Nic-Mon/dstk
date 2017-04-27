b6d39bfc75db6e8558f57f4705af0a18eaec3ee5
Nic-Mon <n_mon@berkeley.edu>
2017-04-26 17:27:31 -0700

    changed array equals in test

--- dstk/tests/test_matrix_ops.py
+++ dstk/tests/test_matrix_ops.py
@@ -5,4 +5,4 @@ import numpy as np
 def test_oneHotEncode():
 	data = ['hello', 'goodbye', 'hello', 'later']
 	matrix = np.matrix([[True, False, True, False], [False, True, False, False], [False, False, False, True]])
-	assert one_hot_encode(data) == matrix
+	assert np.array_equals(one_hot_encode(data), matrix)
