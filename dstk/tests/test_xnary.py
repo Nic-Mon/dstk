942b82ef1a646bf758c8cc0f138cd6c7ec4e4167
Nic-Mon <n_mon@berkeley.edu>
2017-04-27 14:53:06 -0700

    fixed import

--- dstk/tests/test_xnary.py
+++ dstk/tests/test_xnary.py
@@ -5,12 +5,12 @@ def test_canInstantiateBinary():
 	binary_num = xnary(2, 42)
 
 def test_canInstantiate5nary():
-	fivenary = xnary(5,234)
+	fivenary = xnary(5, 234)
 
 def test_binaryToString():
-	binary_fifty = xnary(2,50)
+	binary_fifty = xnary(2, 50)
 	assert(str(binary_fifty) == '110010')
 
 def test_4narytoString():
-	fournary_fortytwo = xnary(4,242)
+	fournary_fortytwo = xnary(4, 242)
 	assert(str(fournary_fortytwo) == '3302')
