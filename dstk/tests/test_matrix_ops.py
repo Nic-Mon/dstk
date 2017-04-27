bb886b2a26af0a51063f62ee0e6c025f7df8e91c
Nic-Mon <n_mon@berkeley.edu>
2017-04-26 17:04:35 -0700

    fixed one hot test

--- dstk/tests/test_matrix_ops.py
+++ dstk/tests/test_matrix_ops.py
@@ -1,7 +1,7 @@
 import pytest
 from dstk import matrix_ops
 
-def test_oneHotEncode(data):
+def test_oneHotEncode():
 	data = ['hello', 'goodbye', 'hello', 'later']
 	matrix = np.matrix([True, False, True, False], [False, True, False, False], [False, False, False, True])
 	assert one_hot_encode(data) == matrix
