2118a02cfb05c81035df99e3b5ea0b01d3d5df40
Nic-Mon <n_mon@berkeley.edu>
2017-04-27 13:38:04 -0700

    abstracted binary class to xnary, added to string method

--- dstk/xnary.py
+++ dstk/xnary.py
@@ -1,12 +1,21 @@
 from math import log
 
-class binary():
+class xnary():
 	'class to understand X-nary number representations (i.e. binary, trinary, etc.)'
 
-	def __init__(self, value):
+	def __init__(self, n, value):
+		self.n = n
 		self.value = value
 
 	def __int__(self):
 		return value
 
-
+	def __str__(self):
+		x = self.value
+		if x == 0:
+        	return '0'
+    	nums = []
+    	while x:
+        	x, r = divmod(x, self.n)
+        	nums.append(str(r))
+    	return ''.join(reversed(nums))
