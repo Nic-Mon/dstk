d847a0b5d91edd0d68107fda07a1d2cb16e76f5e
Nic-Mon <n_mon@berkeley.edu>
2017-04-27 21:20:38 -0700

    implemented row reduce

--- dstk/matrix_ops.py
+++ dstk/matrix_ops.py
@@ -14,4 +14,14 @@ def one_hot_encode(data):
 
 def row_reduce(matrix):
 	'''returns row reduced matrix'''
+	m, n = matrix.shape
+	print(m, n)
+	for k in range(min(m,n)):
+		i_max  = argmax (abs(matrix[k:,k]))
+		matrix[[k, i_max]] = matrix[[i_max, k]]
+		for i in range(k, m):
+			f = matrix[i, k] / matrix[k, k]
+			for j in range(k, n):
+				matrix[i,j] = matrix[i,j] - matrix[k,j]*f
+			matrix[i,k] = 0
 	return matrix
