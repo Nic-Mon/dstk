f091c111b76dc4eeff05deea8ca53032e90745f8
Nic-Mon <n_mon@berkeley.edu>
2017-04-27 23:05:22 -0700

    added is reduced row echelon test

--- dstk/tests/test_matrix_ops.py
+++ dstk/tests/test_matrix_ops.py
@@ -21,3 +21,10 @@ def test_isRowEchelon():
 def test_rowEchelon():
 	matrix1 = np.matrix([[1, 3, 1, 9], [1, 1, -1, 1], [3, 11, 5, 35]])
 	assert (is_row_echelon(row_echelon_form(matrix1)))
+
+def test_isReducedRowEchelon():
+	matrix1 = np.matrix([[3,-9,12,-9,6,15], [0,2,-2,2,1,-3], [0,0,0,0,1,4]])
+	matrix2 = np.matrix([[1,-3,6,-9,6,15], [0,1,-2,5,1,-3], [0,0,0,0,1,4]])
+
+	assert(not is_reduced_row_echelon(matrix1))
+	assert(is_reduced_row_echelon(matrix2))
