e5286b9163489e83fee6120708e70602a5646137
Nic-Mon <n_mon@berkeley.edu>
2017-04-27 18:17:56 -0700

    travis you there?

--- dstk/matrix_ops.py
+++ dstk/matrix_ops.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def one_hot_encode(data):
 	'''takes list of data, returns data one hot encoded into a numpy matrix'''
-	unique_elements = reversed(list(set(data)))
+	unique_elements = list(reversed(list(set(data))))
 	print(unique_elements)
 	matrix = []
 	for item in unique_elements:
