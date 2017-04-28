7e23cec3ad2ed196e98cc5068971436c5c224c19
Nic-Mon <n_mon@berkeley.edu>
2017-04-27 18:24:56 -0700

    fixed onehot test to reflect sortedness

--- dstk/tests/test_matrix_ops.py
+++ dstk/tests/test_matrix_ops.py
@@ -4,5 +4,5 @@ import numpy as np
 
 def test_oneHotEncode():
 	data = ['hello', 'goodbye', 'hello', 'later']
-	matrix = np.matrix([[True, False, True, False], [False, True, False, False], [False, False, False, True]])
+	matrix = np.matrix([[False, True, False, False], [True, False, True, False], [False, False, False, True]])
 	assert np.array_equal(one_hot_encode(data), matrix)
