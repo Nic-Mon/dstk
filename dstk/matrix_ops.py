8d2e245e2634db7c2ef72ee90bba0c9cb740f10f
Nic-Mon <n_mon@berkeley.edu>
2017-04-27 21:46:14 -0700

    added more print statements

--- dstk/matrix_ops.py
+++ dstk/matrix_ops.py
@@ -19,8 +19,11 @@ def row_reduce(matrix):
 		print(k)
 		print(matrix[k:,k])
 		i_max  = np.argmax (abs(matrix[k:,k]))
+		print(i_max)
+		print(i_max is None)
 		i_max = k+i_max
 		matrix[[k, i_max]] = matrix[[i_max, k]]
+		print(matrix)
 		for i in range(k, m):
 			f = matrix[i, k] / matrix[k, k]
 			for j in range(k, n):
