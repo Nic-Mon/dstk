a66306e89422f167f36a1c21302e0996df1d0db6
Nic-Mon <n_mon@berkeley.edu>
2017-04-27 22:57:53 -0700

    added row echelon test

--- dstk/tests/test_matrix_ops.py
+++ dstk/tests/test_matrix_ops.py
@@ -1,17 +1,13 @@
 import pytest
 from dstk.matrix_ops import one_hot_encode
 from dstk.matrix_ops import is_row_echelon
+from dstk.matrix_ops import row_echelon_form
 import numpy as np
 
 def test_oneHotEncode():
 	data = ['hello', 'goodbye', 'hello', 'later']
 	matrix = np.matrix([[False, True, False, False], [True, False, True, False], [False, False, False, True]])
 	assert np.array_equal(one_hot_encode(data), matrix)
-
-# def test_rowReduce():
-# 	matrix1 = np.matrix([[1, 3, 1, 9], [1, 1, -1, 1], [3, 11, 5, 35]])
-# 	matrix2 = np.matrix([[1, 0, -2, -3], [0, 1, 1, 4], [0, 0, 0, 0]])
-# 	assert (row_reduce(matrix1) == matrix2)
 
 def test_isRowEchelon():
 	matrix1 = np.matrix([[3,-9,12,-9,6,15], [0,1,-2,2,1,-3], [0,0,0,0,1,4]])
@@ -22,3 +18,6 @@ def test_isRowEchelon():
 	assert(not is_row_echelon(matrix2))
 	assert(not is_row_echelon(matrix3))
 
+def test_rowEchelon():
+	matrix1 = np.matrix([[1, 3, 1, 9], [1, 1, -1, 1], [3, 11, 5, 35]])
+	assert (is_row_echelon(row_echelon_form(matrix1)))
